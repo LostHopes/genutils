12a68c47367212e48edadf3b7148517cfee82aed
losthope <arsen.melnuchyk@gmail.com>
2025-04-19 00:18:55 +0300

    chore: remove cpg for now

--- src/cpg.h
+++
@@ -1,7 +0,0 @@
-bool isDir(const char *path);
-bool checkExists(const char *path);
-size_t copyFile(const char* fileIn, const char* dir);
-size_t copyDir(const char* pathIn, const char* pathOut);
-const char* usage();
-void printVerbose();
-void parseArgs(int argc, char** argv);
