8ac33033326f7822eb649d4c4990f1fa68b2abfe
losthope <arsen.melnuchyk@gmail.com>
2025-05-10 10:56:17 +0300

    refactor: add getMetadata func and init struct

--- src/catg.h
+++ src/catg.h
@@ -1,4 +1,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+struct Flags{
+    bool colorful;
+    bool line_counter;
+}flag;
+
 int readFile(const char* filename);
+int getMetadata(const char* filename);
